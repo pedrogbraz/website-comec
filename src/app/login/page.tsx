c5709bc8b754ea3e88e3152cf1f171e1637d55c7
pedrogbraz <pedrobrazdevs@gmail.com>
2024-07-09 00:23:53 -0300

    feat: add sign up and sign in page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,15 +5,8 @@ import Link from "next/link";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
 import { Checkbox } from "@/components/ui/checkbox"
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 
 export default function Login() {
   const [cpf, setCpf] = useState<string>('');
