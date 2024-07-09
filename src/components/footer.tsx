40040bd728f39b2da4e8142f2e6c9464bac68f0e
pedrogbraz <pedrobrazdevs@gmail.com>
2024-07-09 00:10:34 -0300

    feat: add footer component

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="flex items-center justify-center w-full border-t py-8 mt-12">
+      <p className="text-[--color-text]">Copyright © 2022. Todos os direitos reservados.</p>
+    </footer>
+  )
+}
