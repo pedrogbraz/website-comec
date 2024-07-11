97871c2e5971c5d44aeb0b0388fb09e172813b5a
Seu Nome <pedrobrazdevs@gmail.com>
2024-07-10 21:02:38 -0300

    feat: add waves to background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
-import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -26,7 +25,6 @@ export default function RootLayout({
             disableTransitionOnChange
           >
         {children}
-        <Footer />
         </ThemeProvider>
       </body>
     </html>
--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          "flex h-9 w-full rounded-md bg-transparent px-2 py-4 md:px-2 md:py-1 text-sm shadow-sm text-[--color-text] md:text-xs transition-colors file:bg-transparent file:text-sm file:font-medium placeholder:text-[--color-text] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+          "flex h-9 w-full rounded-md border-gray-500/15 bg-[--inputLogin] px-2 py-4 md:px-2 md:py-1 text-sm shadow-sm text-[--color-text] md:text-xs transition-colors file:bg-transparent file:text-sm file:font-medium placeholder:text-[--color-text] focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
           className
         )}
         ref={ref}
