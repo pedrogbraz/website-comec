af9304a3377c6d4ddd4fc8b6b82556659e158f34
pedrogbraz <pedrobrazdevs@gmail.com>
2024-08-05 23:56:45 -0300

    fix: remove background with bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,20 +12,9 @@ import { useTheme } from "next-themes";
 export default function Home() {
   const { theme } = useTheme(); // Pegue o tema atual
 
-  const backgroundImage = theme === 'dark' ? '/ondas2.png' : '/ondas1.png';
-
   return (
     <>
       <main className="w-[100vw] h-[100vh] flex flex-col items-center mb-16">
-        <div className="flex -z-50 absolute w-full h-full opacity-35">
-          <Image
-            src={backgroundImage}
-            alt="Teste"
-            layout="fill"
-            objectFit="cover"
-            className=""
-          />
-        </div>
         <Navbar />
         <Inicio />
       </main>
